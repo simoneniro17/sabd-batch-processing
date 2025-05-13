fa49de5dbf5dceb70b64fcc4e4a363bfa6025403
simoneniro17 <simoneniro17@gmail.com>
2025-05-13 18:06:32 +0200

    Refactor e pulizia

--- nifi/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Avvia lo script di importazione del template in background
-chmod +x /opt/nifi/scripts/import-template.sh
-/opt/nifi/scripts/import-template.sh &
-
-# Avvia NiFi in foreground (così Docker non spegne il container)
-exec /opt/nifi/nifi-current/bin/nifi.sh run 
--- nifi/nifi_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker exec -it nifi bash -c "
-chmod +x /opt/nifi/scripts/import-template.sh && \
-/opt/nifi/scripts/import-template.sh
-"
