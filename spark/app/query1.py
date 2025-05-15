ca0beaf58c5e6f7f7bb009739a9933c2a8ba220b
simoneniro17 <simoneniro17@gmail.com>
2025-05-15 22:18:02 +0200

    Bug fix

--- spark/app/query1.py
+++ spark/app/query1.py
@@ -12,13 +12,13 @@ def process_file(spark, path, zone_id):
     df = df.withColumn("Year", year("Datetime (UTC)"))
 
     agg_df = df.groupBy("Year").agg(
-        avg(col("Carbon_intensity_gCO_eq_kWh__direct_")).alias("carbon-avg"),
-        min(col("Carbon_intensity_gCO_eq_kWh__direct_")).alias("carbon-min"),
-        max(col("Carbon_intensity_gCO_eq_kWh__direct_")).alias("carbon-max"),
+        avg(col("Carbon_intensity_gCO_eq_kWh__direct_").cast("float")).alias("carbon-avg"),
+        min(col("Carbon_intensity_gCO_eq_kWh__direct_").cast("float")).alias("carbon-min"),
+        max(col("Carbon_intensity_gCO_eq_kWh__direct_").cast("float")).alias("carbon-max"),
 
-        avg(col("Carbon_free_energy_percentage__CFE__")).alias("cfe-avg"),
-        min(col("Carbon_free_energy_percentage__CFE__")).alias("cfe-min"),
-        max(col("Carbon_free_energy_percentage__CFE__")).alias("cfe-max")
+        avg(col("Carbon_free_energy_percentage__CFE__").cast("float")).alias("cfe-avg"),
+        min(col("Carbon_free_energy_percentage__CFE__").cast("float")).alias("cfe-min"),
+        max(col("Carbon_free_energy_percentage__CFE__").cast("float")).alias("cfe-max")
     )
 
     return agg_df.withColumn("Zone_id", lit(zone_id))
@@ -48,7 +48,7 @@ def main(input_it, input_se, output_path):
             "cfe-avg",
             "cfe-min",
             "cfe-max"
-        )
+        ).orderBy("Zone_id", "Year")
 
         final_df.show()
 
